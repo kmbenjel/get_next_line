d860697ed6c27abee6bed385aa17c78953de3ca8
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-02 00:13:10 +0100

    kbenjell

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,14 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:21 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/02 00:01:38 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/02 00:08:13 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
 
 char	*get_next_line(int fd);
 
