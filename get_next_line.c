fa2da409aae7045758f70e91b5032074f9718ad5
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-28 01:29:15 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,19 +6,26 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/28 00:31:35 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/28 01:29:03 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
+char	*read_bs(int fd)
+{
+	char	*current;
+
+	current = malloc(BUFFER_SIZE);
+	if (current)
+	{
+	}
+	return (NULL);
+}
 char	*get_next_line(int fd)
 {
 	static char			*line;
 	static unsigned int	position;
 
-	line = "";
 	position = 0;
-	if ()
-		return (NULL);
 	return (line);
 }
