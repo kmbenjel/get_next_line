b091b240d61756153c800d563f717945c4d339da
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-28 00:03:45 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,11 +6,15 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/27 21:30:23 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/28 00:03:03 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
 
 char	*get_next_line(int fd)
 {
+	static char			*line;
+	static unsigned int	position;
+
+	line = "";
 }
