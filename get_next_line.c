70356836e39b18ab841b7130d8cf8b6a1a2e187f
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-29 00:54:41 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/29 00:44:04 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/29 00:53:29 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -14,8 +14,11 @@
 static int	new_line_in(char *b)
 {
 	while (*b)
+	{
 		if (*b == '\n')
 			return (1);
+		b++;
+	}
 	return (0);
 }
 
