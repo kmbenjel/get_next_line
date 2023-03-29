c3f6ed6fd412f91b48794a7409405630d0d821e9
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-29 00:19:25 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/29 00:11:28 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/29 00:16:43 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -53,6 +53,7 @@ static char	*joinline(int fd)
 	char	*cb;
 	char	*line;
 
+	line = NULL;
 	cb = current_buffer(fd);
 	while (no_new_line_in(cb))
 	{
@@ -61,10 +62,8 @@ static char	*joinline(int fd)
 	}
 	if (new_line_in(cb))
 	{
-		sep = ft_split(cb, '\n');
 		line = ft_strjoin(line, until_nl(cb));
 		tail = after_nl(cb);
-		free(sep);
 	}
 	return (line);
 }
@@ -73,6 +72,5 @@ char	*get_next_line(int fd)
 {
 	static char	*tail;
 
-	line = ft_strjoin(tail, line);
 	return (line);
 }
