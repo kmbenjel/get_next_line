e1abd4b603a186f894c440bf32b1eec85b5b6a0e
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-29 06:36:33 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/29 06:28:57 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/29 06:34:37 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -67,7 +67,7 @@ static char	*joinline(int fd, char **line, int *rc)
 char	*get_next_line(int fd)
 {
 	static char	*tail;
-	int			*rc;
+	int			rc;
 	static int	pos;
 	char		*line;
 
@@ -79,7 +79,9 @@ char	*get_next_line(int fd)
 		line = ft_strjoin(tail, NULL);
 		free(tail);
 	}
-	tail = joinline(fd, &line, rc);
+	tail = joinline(fd, &line, &rc);
 	pos++;
 	return (line);
 }
+
+// rc is for Read Count
