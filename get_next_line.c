d9f7ea40ea49c98660ef7f3c23c34803744cba6f
Khalid Benjelloun <khalidmailing@gmail.com>
2023-03-29 02:00:08 +0000

    kbenjell

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: kbenjell <kbenjell@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/01 23:59:14 by kbenjell          #+#    #+#             */
-/*   Updated: 2023/03/29 01:54:42 by kbenjell         ###   ########.fr       */
+/*   Updated: 2023/03/29 01:58:56 by kbenjell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "get_next_line.h"
@@ -36,7 +36,7 @@ static char	*current_buffer(int fd, char *cb)
 	int	i;
 
 	i = read(fd, cb, BUFFER_SIZE);
-	if (i <= 0)
+	if (i < 0)
 		return (NULL);
 	cb[BUFFER_SIZE] = '\0';
 	return (cb);
@@ -78,7 +78,10 @@ char	*get_next_line(int fd)
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	if (pos > 0)
+	{
 		line = ft_strjoin(tail, line);
+		free(tail);
+	}
 	tail = joinline(fd, &line);
 	pos++;
 	return (line);
